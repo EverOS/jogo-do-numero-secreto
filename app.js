6863d4353d34942166a4d5728a1f2737dd531d1c
Everton Oliveira <everton.bps@gmail.com>
2025-10-03 15:45:05 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
@@ -76,4 +76,5 @@ function reiniciarJogo(){
     tentativas = 1;
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true);
+
 }
